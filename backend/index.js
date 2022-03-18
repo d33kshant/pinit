5bd6faeb6ab496cae375c82874afbd7a345488f9
Deekshant Yadav <d33kshant@gmail.com>
2022-03-18 23:07:14 +0530

    Add pin route to app

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const pool = require('./db')
 
 const authRoute = require('./routes/auth.route')
+const pinRoute = require('./routes/pin.route')
 
 const PORT = process.env.PORT || 5000
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
+app.use('/api/pin', pinRoute)
 
 pool.connect().then(res=>{
 	app.listen(PORT, async () => {
