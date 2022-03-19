4b5de79df58cfde63afa702058ad14ed33b49a53
Deekshant Yadav <d33kshant@gmail.com>
2022-03-19 16:13:03 +0530

    Add function to get pins

--- backend/controllers/pin.controller.js
+++ backend/controllers/pin.controller.js
@@ -50,7 +50,7 @@ const getPinById = async (req, res) => {
 		const pid = parseInt(id)
 		if (pid) {
 			const query_result = await pool.query(
-				'SELECT pin.id AS pid, title, link, username, created_on FROM pin INNER JOIN "user" ON "user".id=pin.author WHERE pin.id=$1;',
+				'SELECT pin.id AS pid, title, link, username as author, created_on FROM pin INNER JOIN "user" ON "user".id=pin.author WHERE pin.id=$1;',
 				[pid]
 			)
 			if (query_result.rowCount > 0) {
@@ -74,4 +74,34 @@ const getPinById = async (req, res) => {
 	}
 }
 
-module.exports = { create, getPinById }
+/**
+ * Get pins from the database.
+ * @param {Request} req Request object
+ * @param {Response} res Response object
+ */
+const getPins = async (req, res) => {
+	const page = req.query.page || 1
+	const limit = req.query.limit || 10
+
+	if (page <= 0 || limit <= 0) {
+		return res.json({
+			error: "Invalid page or limit."
+		})
+	}
+
+	const offset = (page-1) * limit
+	
+	try {
+		const query_result = await pool.query(
+			'SELECT pin.id AS pid, title, link, username as author, created_on FROM "user" INNER JOIN pin ON "user".id=pin.author ORDER BY created_on DESC LIMIT $1 OFFSET $2;',
+			[limit, offset]
+		)
+		res.json(query_result.rows)
+	} catch (error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
+}
+
+module.exports = { create, getPinById, getPins }
