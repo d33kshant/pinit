ed744830e34bab948a3e18bed6c2de75404f5cff
Deekshant Yadav <d33kshant@gmail.com>
2022-03-19 16:13:24 +0530

    Add route for pins

--- backend/routes/pin.route.js
+++ backend/routes/pin.route.js
@@ -1,9 +1,10 @@
 const { verify } = require('../controllers/auth.controller')
-const { create, getPinById } = require('../controllers/pin.controller')
+const { create, getPinById, getPins } = require('../controllers/pin.controller')
 
 const router = require('express').Router()
 
 router.post('/', verify, create)
+router.get('/', getPins)
 router.get('/:id', getPinById)
 
 
