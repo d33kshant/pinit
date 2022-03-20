1e5a8814a5efb2cb6682b88b59e89f17eb49b085
Deekshant Yadav <d33kshant@gmail.com>
2022-03-20 19:24:55 +0530

    Create AppBar

---
+++ frontend/src/components/AppBar.js
@@ -0,0 +1,22 @@
+import '../styles/AppBar.css'
+
+const AppBar = () => {
+	return (
+		<nav className='appbar-container'>
+			<div className="appbar-logo">
+			<svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48"></path></svg>
+			</div>
+			<form className="appbar-search">
+				<input type="text" className="search-input" placeholder='Search' />
+				<button className='search-button'>
+					<svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+				</button>
+			</form>
+			<div className="appbar-account">
+				<svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+			</div>
+		</nav>
+	)
+}
+
+export default AppBar
