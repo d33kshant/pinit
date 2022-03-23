f23e7ad4b32d4862b62d2183cb5fa4a7469efd10
Deekshant Yadav <d33kshant@gmail.com>
2022-03-23 23:00:11 +0530

    Add input value and chage listener

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -1,13 +1,13 @@
 import '../styles/AppBar.css'
 
-const AppBar = () => {
+const AppBar = ({ searchValue, onSearchValueChange }) => {
 	return (
 		<nav className='appbar-container'>
 			<div className="appbar-logo">
 			<svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48"></path></svg>
 			</div>
 			<form className="appbar-search">
-				<input type="text" className="search-input" placeholder='Search' />
+				<input value={searchValue} onChange={onSearchValueChange} type="text" className="search-input" placeholder='Search' />
 				<button className='search-button'>
 					<svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
 				</button>
