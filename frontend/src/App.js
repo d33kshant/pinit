343f4710901405ff9c5152e142e14a12deb36ea6
Deekshant Yadav <d33kshant@gmail.com>
2022-03-23 23:00:43 +0530

    Add pin's grid and search feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,47 @@
+import { useEffect, useState } from 'react'
+import Masonry from 'react-masonry-css'
 import AppBar from './components/AppBar'
+import Pin from './components/Pin'
 import './styles/App.css'
 
 function App() {
+	
+	const [pins, setPins] = useState([])
+	const [query, setQuery] = useState('')
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const search = query ? `search=${query.replace(' ', '%20')}` : ''
+
+		fetch(`/api/pin?limit=20&${search}`).then(res=>res.json())
+		.then(res => {
+			if (res.error) {
+				return alert(res.error)
+			}
+			setPins(res)
+			setLoading(false)
+		})
+	}, [query])
+
 	return (
 		<div className='app'>
-			<AppBar />
+			<AppBar searchValue={query} onSearchValueChange={(event)=>setQuery(event.target.value)} />
+			{
+				loading ?
+				'Loading...' :
+				pins.length <= 0 ?
+				'No pins found' :
+				<Masonry
+					breakpointCols={{
+						default: 6,
+						768: 4,
+						430: 2,
+					}}
+					className="pins-grid"
+					columnClassName="pins-grid-column">
+					{ pins.map(pin=><Pin data={pin} />) }
+				</Masonry>
+			}
 		</div>
 	)
 }
